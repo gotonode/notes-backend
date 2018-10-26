9ab395591e7d47b040f32af932b002779fcc5c9d
gotonode <5214152+gotonode@users.noreply.github.com>
2018-10-26 18:12:32 +0300

    Deploy to Heroku

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
-const notes = [{
+app.use(bodyParser.json())
+
+const logger = (request, response, next) => {
+	console.log("Method:   ", request.method)
+	console.log("Path:     ", request.path)
+	console.log("Body:     ", request.body)
+	console.log("---")
+	next()
+}
+
+let notes = [{
 		id: 1,
 		content: "First",
 		date: "2018",
@@ -29,8 +40,56 @@ app.get('/notes', (req, res) => {
 	res.json(notes)
 })
 
+app.get('/notes/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const note = notes.find(note => {
+		// console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+		return note.id === id
+	})
 
-const PORT = 3000;
+	if (note) {
+		res.json(note)
+	} else {
+		res.status(404).end()
+	}
+})
+
+const generateId = () => {
+	const maxId = notes.length > 0 ? notes.map(n => n.id).sort((a, b) => a - b).reverse()[0] : 1
+	return maxId + 1
+}
+
+app.post('/notes', (req, res) => {
+	const body = req.body
+	//console.log(note)
+	//console.log(req.headers)
+
+	if (body.content === undefined) {
+		return res.status(400).json({
+			error: "Content is missing."
+		})
+	}
+
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+		id: generateId()
+	}
+
+	notes = notes.concat(note)
+
+	res.json(note)
+})
+
+app.delete('/notes/:id', (req, res) => {
+	const id = Number(req.params.id)
+	notes = notes.filter(note => note.id !== id)
+
+	res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}.`)
